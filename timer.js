0ca2f167b2b4cb905fb7e30fe3c50998cd88588f
Leskovets Maksym <leskovecmaksim@gmail.com>
2021-06-07 01:14:55 +0200

    Fixed hw11 timer (rework to class)

--- timer.js
+++ timer.js
@@ -1,33 +1,55 @@
-const days = document.querySelector('[data-value="days"]');
-const hours = document.querySelector('[data-value="hours"]');
-const minutes = document.querySelector('[data-value="mins"]');
-const seconds = document.querySelector('[data-value="secs"]');
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.updateClockView = this.getClockValue(selector);
+    this.targetDate = targetDate;
+    this.timer();
+    this.time();
+  };
 
-const countDownDate = new Date("Jun 13, 2021 23:59:59").getTime();
+  getClockValue(time) {
+    const refs = {
+      days: document.querySelector(`${time} [data-value="days"]`),
+      hours: document.querySelector(`${time} [data-value="hours"]`),
+      minutes: document.querySelector(`${time} [data-value="mins"]`),
+      seconds: document.querySelector(`${time} [data-value="secs"]`),
+    }
+    return refs;
+  };
 
-function time() {
-  const now = new Date().getTime();
+  time() {
+    const distance = this.targetDate - new Date();
 
-  const distance = countDownDate - now;
+    const d = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const s = Math.floor((distance % (1000 * 60)) / 1000);
+  
 
-  const d = Math.floor(distance / (1000 * 60 * 60 * 24));
-  const h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  const s = Math.floor((distance % (1000 * 60)) / 1000);
+    this.updateClockView.days.innerHTML = d;
+    this.updateClockView.hours.innerHTML = h;
+    this.updateClockView.minutes.innerHTML = m;
+    this.updateClockView.seconds.innerHTML = s;
 
-    days.innerHTML = d;
-    hours.innerHTML = h;
-    minutes.innerHTML = m;
-    seconds.innerHTML = s;
-
-  if (distance < 0) {
-    clearInterval(timer);
-    document.getElementById("demo").innerHTML = "time is up!";
+      if (this.targetDate <= new Date()) {
+        this.updateClockView.days.innerHTML = `00`;
+        this.updateClockView.hours.innerHTML = `00`;
+        this.updateClockView.minutes.innerHTML = `00`;
+        this.updateClockView.seconds.innerHTML = `00`;
     };
+  };
+
+  timer() {
+    const timerId = setInterval(() => {
+      this.time();
+
+        if (this.targetDate <= new Date()) {
+          clearInterval(timerId);
+      };
+    }, 1000);
+  };
 };
 
-const timer = setInterval(function () {
-    time();
-}, 1000);
-
-time();
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jun 13, 2021 23:59:59'),
+});
